3b08d404d03189642508ea74adde5d27ecb077c6
iGSUruguay <30572826+iGSUruguay@users.noreply.github.com>
2018-04-12 13:14:38 -0300

    Cambios Varios

--- One.Services.Paradise/Program.cs
+++ One.Services.Paradise/Program.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using Syncfusion.Pdf.Graphics;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static One.Services.Paradise.Printer.Helper;
 
 namespace One.Services.Paradise
 {
@@ -10,6 +12,27 @@ namespace One.Services.Paradise
     {
         static void Main(string[] args)
         {
+            Report report = new Report()
+            {
+                PageSize = new Report.ReportPageSize() { Width = 8, Height = 15 },
+                Pages = new Report.ReportPage[]
+                {
+                    new Report.ReportPage()
+                    {
+                        Data = new object[]
+                        {
+                            new Report.Text()
+                            {
+                                Data = "Hola",
+                                Font =new PdfStandardFont(PdfFontFamily.Helvetica, 20),
+                                Brush = PdfBrushes.DarkRed
+                            }
+                        }
+                    }
+                }
+            };
+            PrintTest(Newtonsoft.Json.JsonConvert.SerializeObject(report));
+            Console.ReadLine();
         }
     }
 }
